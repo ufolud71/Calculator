3f6831c3ab0f9bb74011520680a9dbc38336b97d
ufolud71 <chudy.orfeusz@gmail.com>
2019-08-13 15:55:55 +0200

    Calculator

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -1,4 +1,26 @@
 package com.kodilla.calculator;
 
 public class Calculator {
+
+    int a;
+    int b;
+
+    public Calculator( int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public int addition () {
+        System.out.println("It Works!!");
+        return a + b;
+    }
+    public int substraction () {
+        System.out.println("It also works!!");
+        return a - b;
+    }
+
+    public static void main(String args[]) {
+       Calculator calculator = new Calculator(10, 5);
+       calculator.addition();
+       calculator.substraction();
+    }
 }
